e65383c51751cb999a3f971d85ffce1f82d7f93e
fredlabbe <47671987+fredlabbe@users.noreply.github.com>
2021-04-13 00:01:41 -0400

    Put arena

--- FinalProject315/Assets/Scripts/PlayerManager.cs
+++ FinalProject315/Assets/Scripts/PlayerManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    #region Singleton 
+    #region Singleton  
 
     public static PlayerManager instance;
 
@@ -16,4 +16,41 @@ public class PlayerManager : MonoBehaviour
     #endregion 
 
     public GameObject player;
+    public Camera mainCamera;
+
+    private float range = 3f;
+
+    int delay = 50;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            
+            delay += 1;
+            if (delay >= 15)
+            {
+                delay = 0;
+                Ray clickRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit result;
+                if (Physics.Raycast(clickRay, out result) && result.collider.tag == "Enemy")
+                { 
+                    if(result.distance > 1.5)
+                    {
+                        //GameObject.Instantiate(player, result.point, Quaternion.identity); 
+                        //Attack enemy  
+                        Debug.Log("Plyaer Attacking");
+                    }
+                    
+                }
+            }
+
+        }
+        else
+        {
+            delay = 50;
+        }
+    }
 }
